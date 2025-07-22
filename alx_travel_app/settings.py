f3368aa7e2aee1ccd404256ec0a0a71ff17f63c6
Muliro1 <mulirokhaemba@gmail.com>
2025-07-22 04:05:55 +0300

    int

--- alx_travel_app/settings.py
+++ alx_travel_app/settings.py
@@ -134,7 +134,7 @@ DATABASES = {
         'USER': env('DB_USER'),
         'PASSWORD': env('DB_PASSWORD'),
         'HOST': env('DB_HOST', default='localhost'),
-        'PORT': env('DB_PORT', default='5432'),
+        'PORT': env('DB_PORT', default=5432),
     }
 }
 
